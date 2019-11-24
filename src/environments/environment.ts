e588695e801b8da6598c0dbd72a16fde6f727ee0
cokagun09 <cokgung09@gmail.com>
2019-11-25 00:41:54 +0800

    Success CRUD firebase Angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig  : {
+    apiKey: "AIzaSyCg6V7mYlw89UgbUpUVaFxBgbEyoLWXXa0",
+    authDomain: "biodata-60f03.firebaseapp.com",
+    databaseURL: "https://biodata-60f03.firebaseio.com",
+    projectId: "biodata-60f03",
+    storageBucket: "biodata-60f03.appspot.com",
+    messagingSenderId: "839655237653",
+    appId: "1:839655237653:web:0827b73a7c85129bd7461c",
+    measurementId: "G-HHK64WS2DX"
+  }
 };
 
 /*
